ed8569b7a3b3e29d550c523f78c6d180af567f24
Kisaragi Hiu <mail@kisaragi-hiu.com>
2019-08-24 01:59:28 +0900

    add support for different fonts

--- pollen.rkt
+++ pollen.rkt
@@ -16,10 +16,12 @@
 
 (define to-html ->html)
 
-(define (word-practice sample-text #:grid? [grid? #t])
+(define (word-practice #:grid? [grid? #t] #:font [font #f]
+                       . sample-text)
   (define grid-or-line (if grid? "with-grid" "with-line"))
-  `(div ([class "word-practice"])
-        (div ([class ,(string-join `("reference" ,grid-or-line))]) ,sample-text)
-        (div ([class ,(string-join `("write-over" ,grid-or-line))]) ,sample-text)
-        (div ([class ,(string-join `("empty" ,grid-or-line))]) ,sample-text)
-        (div ([class "empty"]) ,sample-text)))
+  `(div ([class "word-practice"] ,@(if font `([style ,(format "font-family: '~a';" font)])
+                                       empty))
+        (div ([class ,(string-join `("reference" ,grid-or-line))]) ,@sample-text)
+        (div ([class ,(string-join `("write-over" ,grid-or-line))]) ,@sample-text)
+        (div ([class ,(string-join `("empty" ,grid-or-line))]) ,@sample-text)
+        (div ([class "empty"]) ,@sample-text)))
