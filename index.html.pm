7b2423876b39e66738e9188185d3b3a82f705c48
Kisaragi Hiu <mail@kisaragi-hiu.com>
2019-08-24 01:59:38 +0900

    add default sample

--- index.html.pm
+++ index.html.pm
@@ -1,5 +1,10 @@
 #lang pollen
+◊; Grid is actually kind of broken. I haven't figured out a way to line grid cells up with characters.
 
-◊define[sample-text]{Sample Text}
+◊word-practice[#:grid? #f #:font "Fira Sans"]{
+Sample Text
+}
 
-◊word-practice[#:grid? #f]{Sample Text}
+◊word-practice[#:grid? #f #:font "JK Gothic L"]{
+サンプルです
+}
